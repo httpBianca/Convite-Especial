20b9f155539bc868348a98c0420e3c13b7158690
Bianca Oliveira <113205824+httpBianca@users.noreply.github.com>
2023-06-29 10:24:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const aceitar = document.querySelector('.aceitar');
 
 btt.addEventListener('click', function convite(){
     aceitar.innerHTML = 
-    `Não fez mais do que sua obrigação ao aceitar, agora se liga nas informações!! <br> <b>Lugar:</b> Campo de Futebol (IFCE) <br> <b>Horário:</b> Apartir das <b>16:00 horas </b>; <br>
+    `Não fez mais do que sua obrigação ao aceitar, agora se liga nas informações!! <br> <b>Lugar:</b> Campo de Futebol (IFCE) <br> <b>Horário:</b> Apartir das <b>16:00 horas </b> <br>
     <img src="a_turma_toda.png">`
 });
